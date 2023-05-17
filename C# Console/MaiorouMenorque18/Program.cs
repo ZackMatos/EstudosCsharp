e4a2a754a0b7a79d32a0b34ba11b919150f87070
zackmatos <130622869+zackmatos@users.noreply.github.com>
2023-05-16 23:14:41 -0300

    Estudos Csharp
    Aprendendo a utilizar ForegroundColor no console para mudar a cor das letras.

---
+++ C# Console/MaiorouMenorque18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorouMenorque18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+            while (idade <= 0)
+            {
+                //ForeGroundColor muda a cor das letras.
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Idade inválida,digite uma idade real:");
+                Console.ForegroundColor = ConsoleColor.White;
+                idade = int.Parse(Console.ReadLine());
+            }
+            //Caso a idade da pessoa seja menor que 18 não irá ter permissão
+            if (idade < 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sem permissão");
+            }
+            //Caso contrário (seja maior que 18) terá permissão.
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Permissão concedida");
+            }
+            Console.ReadKey();
+        }
+    }
+}
